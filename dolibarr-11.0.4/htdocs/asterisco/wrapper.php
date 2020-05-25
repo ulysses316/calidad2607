336670637c720525c2b311e5ca806cbd2c1c445a
Vic1007 <65789587+Vic1007@users.noreply.github.com>
2020-05-25 18:09:14 -0500

    Agregue llaves alrededor de las declaraciones anidadas.

--- dolibarr-11.0.4/htdocs/asterisco/wrapper.php
+++ dolibarr-11.0.4/htdocs/asterisco/wrapper.php
@@ -27,14 +27,15 @@
  * 				read = system,call,log,verbose,command,agent,user
  * 				write = system,call,log,verbose,command,agent,user
  */
-
+{
 if (! defined('NOREQUIRESOC'))    define('NOREQUIRESOC', '1');
 if (! defined('NOREQUIRETRAN'))   define('NOREQUIRETRAN', '1');
 if (! defined('NOCSRFCHECK'))     define('NOCSRFCHECK', '1');
 if (! defined('NOTOKENRENEWAL'))  define('NOTOKENRENEWAL', '1');
 if (! defined('NOREQUIREMENU'))   define('NOREQUIREMENU', '1');
 if (! defined('NOREQUIREHTML'))   define('NOREQUIREHTML', '1');
-if (! defined('NOREQUIREAJAX'))   define('NOREQUIREAJAX', '1');
+if (! defined('NOREQUIREAJAX'))   define('NOREQUIREAJAX', '1');	
+}
 
 /**
  * Empty header
