1c0835349274deed1b91760ad87ea1e71a6914eb
dandanny24 <65792332+dandanny24@users.noreply.github.com>
2020-05-27 02:00:23 -0500

    Update
    Corchetes if. Línea 91
    Cochetes else. Línea 93
    Corchetes if. Línea 106

--- dolibarr-11.0.4/htdocs/categories/edit.php
+++ dolibarr-11.0.4/htdocs/categories/edit.php
@@ -106,7 +106,7 @@ if ($action == 'update' && $user->rights->categorie->creer)
 	if (!$error && empty($object->error))
 	{
 		$ret = $extrafields->setOptionalsFromPost(null, $object);
-		if ($ret < 0) $error++;
+		if ($ret < 0) {$error++;}
 
 		if (!$error && $object->update($user) > 0)
 		{
