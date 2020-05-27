62e95b299bc088a3406a7d30b672b7b646f25f43
Alberto7Arce <65825261+Alberto7Arce@users.noreply.github.com>
2020-05-26 23:37:13 -0500

    Update autoloader.php
    [Update] Remove this commented out code. linea 10

--- dolibarr-11.0.4/htdocs/debugbar/class/autoloader.php
+++ dolibarr-11.0.4/htdocs/debugbar/class/autoloader.php
@@ -7,7 +7,7 @@
 spl_autoload_register(function ($class) {
     if (preg_match('/^DebugBar/', $class) || preg_match('/^'.preg_quote('Psr\Log', '/').'/', $class)) {
         $file = DOL_DOCUMENT_ROOT.'/includes/'.str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
-        //var_dump($class.' - '.file_exists($file).' - '.$file);
+        
         if (file_exists($file)) {
     		require_once $file;
     		return true;
